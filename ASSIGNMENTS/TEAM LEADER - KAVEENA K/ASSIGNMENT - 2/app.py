f0fe590850ba34d3438317a43081d44d6127cbc3
kaveena0910 <110366291+kaveena0910@users.noreply.github.com>
2022-10-26 23:01:42 +0530

    Add files via upload

---
+++ ASSIGNMENTS/TEAM LEADER - KAVEENA K/ASSIGNMENT - 2/app.py
@@ -0,0 +1,13 @@
+from operator import imod
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def login():
+    return render_template("login.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
